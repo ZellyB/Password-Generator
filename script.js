880fc6f0cd6781ea6e960fad4b1d170c95682ee3
kelly boothe <kellygageboothe@gmail.com>
2022-11-26 10:08:53 -0500

    entering varables charaters

---
+++ script.js
@@ -0,0 +1,25 @@
+// Assignment code here
+var characterlength = 8;
+var choiceArr = [];
+
+var specialCharArr = [`!`,`@`,`#`,`$`,`_`];
+var lowerCaseArr = [`a`,`b`,`c`,`d`,`e`,`f`,`g`,`h`,`i`,`j`,`k`,`l`,`m`,`n`,`o`,`p`,`r`,`s`,`t`,`u`,`v`,`w`,`x`,`y`,`z`];
+var upperCaseArr =[`A`,`B`,`C`,`D`,`E`,`F`,`G`,`H`,`I`,`J`,`K`,`L`,`M`,`N`,`O`,`P`,`R`,`S`,`T`,`U`,`V`,`W`,`X`,`Y`,`Z`];
+var numbersArr = [`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`];
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {}
